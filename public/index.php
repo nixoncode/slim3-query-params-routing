b7aaf85199f4e34d34e92260add7a74f1edeb596
Nixon Kosgei <nkosgey6@gmail.com>
2020-05-29 05:10:41 +0300

    slim hello World

--- public/index.php
+++ public/index.php
@@ -6,4 +6,19 @@
  * Time: 05:01
  */
 
-echo 'Hello World';
+use Slim\App;
+
+date_default_timezone_set('Africa/Nairobi');
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+$app = new App();
+
+
+$app->get('/', function ($request, $response){
+   $response->write('Hello World');
+});
+
+
+$app->run();
